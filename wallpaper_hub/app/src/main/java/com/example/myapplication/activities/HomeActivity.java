5e0c58b61da3cbd744a5387830ed11bda8252e0a
andr0med0s <andromedoz@gmail.com>
2021-01-08 15:15:21 +0500

    Switching Screens
    19 lessOn

--- wallpaper_hub/app/src/main/java/com/example/myapplication/activities/HomeActivity.java
+++ wallpaper_hub/app/src/main/java/com/example/myapplication/activities/HomeActivity.java
@@ -1,16 +1,57 @@
 package com.example.myapplication.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.myapplication.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+import fragments.HomeFragment;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        displayFragment(new HomeFragment());
+    }
+
+    private void displayFragment (Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_area, fragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment;
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_fav:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_set:
+                fragment = new HomeFragment();
+                break;
+            default:
+                fragment = new HomeFragment();
+                break;
+        }
+        displayFragment(fragment);
+        return true;
     }
 }
